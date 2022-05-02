eb08a209a7e25ce3c12ae9d7c03034b92664eb49
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-05-02 23:01:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", load_style);
 
-//if ('serviceWorker' in navigator) { navigator.serviceWorker.register('sw.js').then(function(registration){registration.update()})}; //experimenteller Offlinemodus
+if ('serviceWorker' in navigator) { navigator.serviceWorker.register('sw.js').then(function(registration){registration.update()})}; //experimenteller Offlinemodus
 
 function load_style(){
     load_radio_script();
