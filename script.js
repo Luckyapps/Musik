4723b9c5464cec2f6d97dc10724b20fa80f7cad6
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-05-02 21:05:16 +0200

    Design test changes

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ window.addEventListener("load", load_style);
 function load_style(){
     //load_titlebar_stylesheet();
     //load_content_stylesheet();
-    if(window.location.href == "file:///C:/Users/ole/Documents/GitHub/Musik/index.html"){
-        load_version_history_stylesheet();
-    }
+    load_version_history_stylesheet();
     load_error_stylesheet();
     //load_sidecard_stylesheet();
     load_cookie_stylesheet();
