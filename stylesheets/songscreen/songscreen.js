61c3d0dbaa24057fa8edb8775323c2b7df66a6f0
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-08-20 16:30:18 +0200

    Songscreen designanpassung

--- stylesheets/songscreen/songscreen.js
+++ stylesheets/songscreen/songscreen.js
@@ -5,12 +5,13 @@ function songscreen_start(){
 }
 
 function songscreen_open(){
-    var content = "<div class='songscreen_container'>"
+    var content = "<div class='songscreen_container_back' style='background: url("+ radio.current_stream.data.image.src +");background-size: cover;background-position: center;'>"
+    +"<div class='songscreen_container'>"
     +"<img src='"+ radio.current_stream.data.image.src +"'>"
+    +"<div class='songscreen_name_container'><h2 class='songscreen_name'>"+ radio.current_stream.data.name +"</h2>"
+    +"<div class='songscreen_description color_sub'>"+ radio.current_stream.data.description +"</div></div>"
     +"<div class='songscreen_playbutton'>></div>"
-    +"<h2 class='songscreen_name'>"+ radio.current_stream.data.name +"</h2>"
-    +"<div class='songscreen_description color_sub'>"+ radio.current_stream.data.description +"</div>"
-    +"</div>";
+    +"</div></div>";
 
     flyin_open(content);
     document.getElementsByClassName("songscreen_playbutton")[0].addEventListener("click", function(){audio_toggle(radio.current_stream.current_button, radio.current_stream.key)})
