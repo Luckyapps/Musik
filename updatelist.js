f17fc3bd1863c5af74a5f27732ddc11c2afe5276
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-10-01 17:12:59 +0200

    updateinfos hinzugefügt

---
+++ updatelist.js
@@ -0,0 +1,16 @@
+var updatelist_musik = {
+    "source": "Musik",
+    "content": [ 
+        {
+            id: "dv.1.22091.0",
+            title: "Luckyapp Update 1|sep22",
+            name:"",
+            date: "27.09.2022",
+            type: "UPDATE",
+            description: "TESETSDGFSDFsdf"
+        }
+    ]
+};
+
+luckyapp_core.modules.updates.updatelists.musik = {loaded: true};
+luckyapp_core.modules.updates.updatelists.musik.list = updatelist_musik;
