ac6c7090c1bd89f02c05c2d9e855d367f859dfb7
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-10-03 11:15:12 +0200

    dv.1.22101.1
    - Bugfix: Es wurde ein Fehler behoben, der dafür gesorgt hat, dass das flyin-Fenster zu hoch geflogen ist, wenn die Seite nach unten gescrollt war.

--- updatelist.js
+++ updatelist.js
@@ -2,12 +2,12 @@ var updatelist_musik = {
     "source": "Musik",
     "content": [ 
         {
-            id: "dv.1.22091.0",
-            title: "Luckyapp Update 1|sep22",
-            name:"",
-            date: "27.09.2022",
+            id: "dv.1.22101.1",
+            title: "Luckyapp Update 1|okt22",
+            name:"Bugfix",
+            date: "03.10.2022",
             type: "UPDATE",
-            description: "TESETSDGFSDFsdf"
+            description: "<ul><li><b>Bugfix:</b> Es wurde ein Fehler behoben, der dafür gesorgt hat, dass das flyin-Fenster zu hoch geflogen ist, wenn die Seite nach unten gescrollt war.</li></ul>"
         }
     ]
 };
