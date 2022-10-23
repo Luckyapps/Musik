39e0a36c45dbb0a523d5a47ca9a0c194087f3903
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-10-23 18:51:24 +0200

    Channel Abschnitt

--- stylesheets/channels/channels.js
+++ stylesheets/channels/channels.js
@@ -1,6 +1,11 @@
+var channels_list_status = "none";
+
 function channels_start(){
     for(i=0;i<document.getElementsByClassName("home_card").length;i++){
         document.getElementsByClassName("home_card")[i].addEventListener("click", channels_start_onclick);
+    }
+    if(channels_list_status != "loaded"){
+        channels_list_load();
     }
 }
 
@@ -44,6 +49,9 @@ function channels_get(stream){
                 return radio.channels[radio.channels_keylist[i]];
             }
         }
+        if(stream == radio.channels_keylist[i]){
+            return radio.channels[radio.channels_keylist[i]];
+        }
     }
 }
 
@@ -56,4 +64,26 @@ function channels_streamlist(stream, channel){
         streamlist.content[channel.streams[i]] = radio.streamlist.base.content[channel.streams[i]];
     }
     return streamlist;
+}
+
+function channels_list_load(){
+    streamlist = {};
+    streamlist.content = radio.channels;
+    streamlist.keylist = Object.keys(streamlist.content);
+    var home_container = document.getElementsByClassName("radio_channels")[0].getElementsByClassName("home_card_container")[0];
+    home_container.innerHTML = "";
+    for(i=0; i < streamlist.keylist.length; i++){
+        if(streamlist.content[streamlist.keylist[i]].radiotext != undefined){
+            var radiotext = " id='"+ streamlist.keylist[i] +"_rtcd'>"+ streamlist.content[streamlist.keylist[i]].radiotext;
+        }else{
+            var radiotext = " id='"+ streamlist.keylist[i] +"_rtcd'>"+ streamlist.content[streamlist.keylist[i]].description;
+        }
+        if(streamlist.content[streamlist.keylist[i]]){
+            home_container.innerHTML += "<div class='home_card' id='"+ streamlist.keylist[i] +"_hc'><div class='home_card_img'><img src='"+ streamlist.content[streamlist.keylist[i]].image.src +"'></div><h3>"+ streamlist.content[streamlist.keylist[i]].name +"</h3><p"+ radiotext +"</p></div>"
+        }else{
+            console.log("nomain");
+        }    
+    }
+    channels_list_status = "loaded";
+    channels_start();
 }
