980aaf59c2ec596589e7468007204d660c97ba94
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2023-03-29 17:33:27 +0200

    setting up for v1.23031.1

--- updatelist.js
+++ updatelist.js
@@ -2,6 +2,13 @@ var updatelist_musik = {
     "source": "Musik",
     "content": [
         {
+            id: "v.1.23031.1",
+            title: "",
+            name: "",
+            date: "29.03.2023",
+            type: "Release",
+            description: "<ul><li><b>Originals</b><ul><li>Unter Originals werden <b>Originalinhalte verfügbar</b> sein.</li><li>Die Seite Originals kann über das Menüband geöffnet werden.</li><li><span style='color:red'><b>Achtung:</b> Funktion noch in einem frühen Entwicklungsstadium.</span></li></ul></li><li>Einstellungen.<ul><li>In den Einstellungen werden jetzt die genauen Versionsinformationen zu einzelnen Updates Angezeigt.</li></ul></li><li>Durch das drücken der Zurück-Taste am Smartphone oder dem zurücknavigieren im Browser wird nun das Flyin geschlossen.</li><li>Experimente mit <b>Radiotext für Radio Sauerland</b> <ul><li>Funktion noch in der Testphase.</li><li>Außerdem in Arbeit: Wenn verfügbar Album cover und andere Informationen auf dem Songscreen anzeigen lassen.</li></ul>"
+        },{
             id: "v.1.23011.1",
             title: "Luckyapp Musik 2",
             name: "Brandneue Version",
